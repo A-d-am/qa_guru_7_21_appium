961253161e3d6782641cd0f33cc7aa11223bba9a
A-d-a-m <adam3928@mail.ru>
2023-11-10 21:13:46 +0200

    added test for onboarding

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -40,3 +40,35 @@ def test_open_article_after_search():
         article_header = browser.element((AppiumBy.CLASS_NAME, "android.widget.TextView"))
 
         assert article_header.should(have.exact_text('Appium'))
+
+@android
+def test_onboarding():
+    continue_button = browser.element((AppiumBy.ID,'org.wikipedia:id/fragment_onboarding_forward_button'))
+    screen_title = browser.element((AppiumBy.ID, 'org.wikipedia:id/primaryTextView'))
+
+    with step('First onboardign screen is opened'):
+        screen_title.should(be.visible)
+
+    with step('Click on continue button to open the second screen'):
+        continue_button.click()
+
+    with step('Second onboarding screen is opened'):
+        screen_title.should(have.exact_text('New ways to explore'))
+
+    with step('Click on continue button to open the third screen'):
+        continue_button.click()
+
+    with step('Third onboarding screen is opened'):
+        screen_title.should(have.exact_text('Reading lists with sync'))
+
+    with step('Click on continue button to open the fourth screen'):
+        continue_button.click()
+
+    with step('Fourth onboarding screen is opened'):
+        screen_title.should(have.exact_text('Send anonymous data'))
+
+    with step('Finish onboarding by clicking on the send anonymous data accept button'):
+        browser.element((AppiumBy.ID,'org.wikipedia:id/acceptButton')).click()
+
+    with step('Main tab is opened'):
+        browser.element((AppiumBy.ID,'org.wikipedia:id/main_toolbar_wordmark')).should(be.visible)
