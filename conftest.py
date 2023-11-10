13316b1a8033431f58425097a526bf49e00f87fc
A-d-a-m <adam3928@mail.ru>
2023-11-10 21:07:16 +0200

    added  if config.is_bstack_run(request.param):

--- conftest.py
+++ conftest.py
@@ -1,70 +1,27 @@
 import allure
 import pytest
 import allure_commons
-from appium.options.android import UiAutomator2Options
-from appium.options.ios import XCUITestOptions
 from selene import browser, support
 from appium import webdriver
+from qa_guru_7_21_appium import utils
 from qa_guru_7_21_appium.utils.allure_utils import (
     attach_bstack_video,
     attach_screenshot,
     attach_page_source
-    )
+)
+import project
 
-from config import config
+
+config = project.Config(_env_file=utils.file.relative_from_root(f'.env.{project.Config().context}'))
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management(request):
-    if request.param == 'android':
-        options = UiAutomator2Options().load_capabilities(
-            {
-                # Specify device and os_version for testing
-                "platformName": request.param,
-                "platformVersion": config.android_platformVersion,
-                "deviceName": config.android_deviceName,
-                # Set URL of the application under test
-                "app": config.android_app_url,
-                'appWaitActivity': 'org.wikipedia.*',
-                # Set other BrowserStack capabilities
-                'bstack:options': {
-                    "projectName": "First Python project",
-                    "buildName": "browserstack-build-1",
-                    "sessionName": "BStack first_test",
-                    # Set your access credentials
-                    "userName": config.bstack_userName,
-                    "accessKey": config.bstack_accessKey,
-                },
-            }
-        )
-
-    elif request.param == 'ios':
-        options = XCUITestOptions().load_capabilities(
-            {
-                # Set URL of the application under test
-                "app": config.ios_app_url,
-                # Specify device and os_version for testing
-                "deviceName": config.ios_deviceName,
-                "platformName": request.param,
-                "platformVersion": config.ios_platformVersion,
-                # Set other BrowserStack capabilities
-                "bstack:options": {
-                    "userName": config.bstack_userName,
-                    "accessKey": config.bstack_accessKey,
-                    "projectName": "First Python Local project",
-                    "buildName": "browserstack-build-1",
-                    "sessionName": "BStack local_test",
-                    "local": "false",
-                },
-            }
-        )
-
     with allure.step('init app session'):
         browser.config.driver = webdriver.Remote(
-            'http://hub.browserstack.com/wd/hub', options=options
+            config.driver_remote_url, options=config.driver_options(request.param)
         )
 
     browser.config.timeout = config.timeout
-    browser.config.driver_options = options
     browser.config._wait_decorator = support._logging.wait_with(
         context=allure_commons._allure.StepContext
     )
@@ -76,11 +33,12 @@ def mobile_management(request):
     attach_screenshot(browser)
     attach_page_source(browser)
 
-    browser.quit()
+    with allure.step('tear down app session with id: ' + session_id):
+        browser.quit()
 
-    attach_bstack_video(session_id)
+    if config.is_bstack_run(request.param):
+        attach_bstack_video(session_id, config.bstack_userName, config.bstack_accessKey)
 
 
 ios = pytest.mark.parametrize('mobile_management', ['ios'], indirect=True)
 android = pytest.mark.parametrize('mobile_management', ['android'], indirect=True)
-
